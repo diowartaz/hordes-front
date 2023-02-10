44b6c2c61982a6c1c9616b6d92a80b224cdb923c
diowartaz <53496307+diowartaz@users.noreply.github.com>
2023-02-10 17:20:41 +0100

    npm update + clear dependencies

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const fallback = require("express-history-api-fallback");
 const app = express();
 const root = `${__dirname}/dist/website2`;
 
-app.use(express.static(__dirname + "/dist/website2"));
+app.use(express.static(root));
 app.use(fallback("index.html", { root }));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "dist/website2/index.html"));
+  res.sendFile(path.join(root + "/index.html"));
 });
 
-app.listen(process.env.PORT || 4000);
+app.listen(process.env.PORT || 4001);
