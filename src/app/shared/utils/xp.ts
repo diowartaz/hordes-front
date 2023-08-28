78416953217b32d63b954d1d1ce3a65637a4297d
unknown <diowartaz@gmail.com>
2023-08-28 18:22:37 +0200

    discord buttons link

--- src/app/shared/utils/xp.ts
+++ src/app/shared/utils/xp.ts
@@ -16,3 +16,13 @@ export function lvlToXp(lvl: number) {
     let xp = Math.E ** ((lvl + 5.9) / 3) - 10
     return Math.floor(xp)
 }
+
+// export function getLvl(xp: any) {
+//     return Math.floor(10.4067 * Math.log(6.62457 + 0.00043575 * xp) - 19.6018);
+//   }
+  
+//   export function getXpForLvl(lvl: any) {
+//     return Math.floor(
+//       (Math.exp((lvl + 19.6018) / 10.4067) - 6.62457) / 0.00043575
+//     );
+//   }
