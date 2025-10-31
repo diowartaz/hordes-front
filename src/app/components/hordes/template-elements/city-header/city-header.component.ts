7eb829fa8aa2dd475c3ec0af0a7a82c5c42aea7a
Victorien de Cremoux <victorien.decremoux@soprasteria.com>
2025-10-31 18:17:36 +0100

    wiki + icon

--- src/app/components/hordes/template-elements/city-header/city-header.component.ts
+++ src/app/components/hordes/template-elements/city-header/city-header.component.ts
@@ -29,7 +29,10 @@ export class CityHeaderComponent implements OnInit {
     this.subscriptions.push(
       this.cityService.userPlayerCityTime$.subscribe((time) => {
         this.time = time;
-        if ((this.time.string == '00h00' || this.time.string == '23h59') && this.cityService.userPlayerState$.getValue() === UserState.PLAYING) {
+        if (
+          (this.time.string == '00h00' || this.time.string == '23h59') &&
+          this.cityService.userPlayerState$.getValue() === UserState.PLAYING
+        ) {
           this.endDay();
         }
       }),
