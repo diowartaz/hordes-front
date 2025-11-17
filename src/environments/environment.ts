40363fe681ea3bfc9e6b4fe33cb5f4ad62e5212b
Victorien de Cremoux <victorien.decremoux@soprasteria.com>
2025-11-17 16:34:31 +0100

    API URL de prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: 'http://localhost:3000/',
-  API_URL_WS: 'wss://localhost:3000/',
-  // API_URL: 'https://diowartaz.onrender.com/',
-  // API_URL_WS: 'wss://diowartaz.onrender.com/'
+  //API_URL: 'http://localhost:3000/',
+  API_URL: 'https://diowartaz.onrender.com/',
 };
